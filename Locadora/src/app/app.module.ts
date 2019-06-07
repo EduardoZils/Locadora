eef115b6df436afbc0a00956485056f9736364c4
LuanaMora <luanamora32@gmail.com>
2019-06-07 19:03:38 -0300

    Adicionado componentes do material

--- Locadora/src/app/app.module.ts
+++ Locadora/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LocacaoComponent } from './cadastros/locacao/locacao.component';
 import { ClienteComponent } from './cadastros/cliente/cliente.component';
 import { VeiculoComponent } from './cadastros/veiculo/veiculo.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule, MatRadioModule, MatButtonModule, 
+  MatTableModule, MatIconModule, MatPaginatorModule, MatSortModule, MatExpansionModule, MatSelectModule, MatDialogModule, MatProgressSpinnerModule } from '@angular/material';
+  import { DatePipe } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -18,9 +25,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+
+    MatInputModule,
+    MatRadioModule,
+    MatButtonModule, 
+    MatTableModule,
+    MatIconModule,
+    HttpClientModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatExpansionModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
   ],
-  providers: [],
+  providers: [HttpClient, DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
