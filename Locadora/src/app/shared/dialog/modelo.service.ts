7ff5afc4a18a4c23df47d9cfb38a63b9e0579e15
LuanaMora <luanamora32@gmail.com>
2019-06-07 21:56:14 -0300

    Service de veiculo e modelo criado

--- Locadora/src/app/shared/dialog/modelo.service.ts
+++ Locadora/src/app/shared/dialog/modelo.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BaseService } from '../base.service';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Modelo } from 'src/app/cadastros/model/modelo';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ModeloService {
+export class ModeloService extends BaseService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    super();
+   }
+
+   save(modelo: any) : Observable<any>{
+    //Primeiro parâmetro == URL
+    //Segundo Parâmetro == BODY - Corpo de cada requisição
+    return this.http.post(environment.urlWebAPI + "Modeloes/", modelo)
+    .catch((error: any) => Observable.throw(error.error));
+  }
+
+  listAll() : Observable<any>{
+    return this.http.get(environment.urlWebAPI + "Modeloes/")
+    .catch((error: any) => Observable.throw(error.error));
+  }
+
+  delete(id: number) : Observable<any>{
+    return this.http.delete(environment.urlWebAPI + "Modeloes/" + id)
+    .catch((error: any) => Observable.throw(error.error));
+  }
+
+  update(modelo: Modelo) : Observable<any>{
+    return  this.http.put(environment.urlWebAPI + "Modeloes/" + modelo.idModelo, modelo)
+    .catch((error: any) => Observable.throw(error.error));
+  }
+
+  getById(id: number) : Observable<any>{
+    return this.http.get(environment.urlWebAPI + "Modeloes/" + id)
+    .catch((error: any) => Observable.throw(error.error));
+  }
 }
--- Locadora/src/app/shared/dialog/veiculo.service.ts
+++ Locadora/src/app/shared/dialog/veiculo.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BaseService } from '../base.service';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Veiculo } from 'src/app/cadastros/model/veiculo';
 
 @Injectable({
   providedIn: 'root'
 })
-export class VeiculoService {
+export class VeiculoService extends BaseService {
 
-  constructor() { }
+
+  constructor(private http: HttpClient) {
+    super();
+   }
+
+   save(veiculo: any) : Observable<any>{
+    //Primeiro parâmetro == URL
+    //Segundo Parâmetro == BODY - Corpo de cada requisição
+    return this.http.post(environment.urlWebAPI + "Veiculoes/", veiculo)
+    .catch((error: any) => Observable.throw(error.error));
+  }
+
+  listAll() : Observable<any>{
+    return this.http.get(environment.urlWebAPI + "Veiculoes/")
+    .catch((error: any) => Observable.throw(error.error));
+  }
+
+  delete(id: number) : Observable<any>{
+    return this.http.delete(environment.urlWebAPI + "Veiculoes/" + id)
+    .catch((error: any) => Observable.throw(error.error));
+  }
+
+  update(veiculo: Veiculo) : Observable<any>{
+    return  this.http.put(environment.urlWebAPI + "Veiculoes/" + veiculo.idVeiculo, veiculo)
+    .catch((error: any) => Observable.throw(error.error));
+  }
+
+  getById(id: number) : Observable<any>{
+    return this.http.get(environment.urlWebAPI + "Veiculoes/" + id)
+    .catch((error: any) => Observable.throw(error.error));
+  }
+
 }
